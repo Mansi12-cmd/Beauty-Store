b133f5fae62b4885c69be5809acab00623c9520d
Your Name <srivastavamansi077@gmail.com>
2024-10-01 03:33:46 +0530

    worked on models

---
+++ Backend/models/user.model.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+    },
+    phone: {
+      type: Number,
+    },
+
+    role: {
+      type: String,
+      default: "user",
+    },
+
+    status: {
+      type: Number,
+      default: 0,
+    },
+
+},
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+userSchema.methods.matchPassword = async function (enteredPasword) {
+  return await bcrypt.compare(enteredPasword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
