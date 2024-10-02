0f11df1c164831e382b9d83c18248c85a3077391
Your Name <srivastavamansi077@gmail.com>
2024-10-02 09:01:28 +0530

    worked on product model and controller

--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -32,8 +32,7 @@ const userSchema = mongoose.Schema(
       type: Number,
       default: 0,
     },
-
-},
+  },
   { timestamps: true }
 );
 
